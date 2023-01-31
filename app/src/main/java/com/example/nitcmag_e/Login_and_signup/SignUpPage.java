e2c879bf61913620b465453c5ccd8bc4c486b138
Atul <atulkumarsethi2001@gmail.com>
2023-02-01 02:34:52 +0530

    second commit

--- app/src/main/java/com/example/nitcmag_e/Login_and_signup/SignUpPage.java
+++ app/src/main/java/com/example/nitcmag_e/Login_and_signup/SignUpPage.java
@@ -82,6 +82,10 @@ public class SignUpPage extends AppCompatActivity {
                 {
                     Toast.makeText(SignUpPage.this, "Please enter a password", Toast.LENGTH_SHORT).show();
                 }
+                if(confirmPassword.getText().toString().isEmpty())
+                {
+
+                }
                 else if(Patterns.EMAIL_ADDRESS.matcher(userEmailId).matches())
                 {
                     boolean check = checkNitcEmail(userEmailId);
