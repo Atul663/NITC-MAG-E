ec4644320c28de899ea1d51f2408589bbe6fe9ef
Atul <atulkumarsethi2001@gmail.com>
2023-02-18 03:39:30 +0530

    incomplete getStartedPage

---
+++ app/src/main/java/com/example/nitcmag_e/getStarted/getStartedPage.java
@@ -0,0 +1,16 @@
+package com.example.nitcmag_e.getStarted;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.nitcmag_e.R;
+
+public class getStartedPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started_page);
+    }
+}
---
+++ app/src/main/java/com/example/nitcmag_e/getStarted/screenItems.java
@@ -0,0 +1,36 @@
+package com.example.nitcmag_e.getStarted;
+
+public class screenItems {
+    String title, description;
+    int img;
+
+    public screenItems(String title, String description, int img) {
+        this.title = title;
+        this.description = description;
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
